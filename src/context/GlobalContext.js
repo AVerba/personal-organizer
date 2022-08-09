07257dde11c64cbbc4171815fcf95aad1127edcc
Oleksii Verbytskyi <alverba88@gmail.com>
2022-08-09 22:00:42 +0300

    added context

---
+++ src/context/GlobalContext.js
@@ -0,0 +1,22 @@
+import { createContext } from 'react';
+
+const GlobalContext = createContext({
+  monthIndex: 0,
+  setMonthIndex: index => {},
+  smallCalendarMonth: 0,
+  setSmallCalendarMonth: index => {},
+  daySelected: null,
+  setDaySelected: day => {},
+  showEventModal: false,
+  setShowEventModal: () => {},
+  dispatchCalEvent: ({ type, payload }) => {},
+  savedEvents: [],
+  selectedEvent: null,
+  setSelectedEvent: () => {},
+  setLabels: () => {},
+  labels: [],
+  updateLabel: () => {},
+  filteredEvents: [],
+});
+
+export default GlobalContext;
