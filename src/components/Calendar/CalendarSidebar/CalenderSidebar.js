c8e26b6cef9a6eedd9b02181f0c8aee9645fa534
Oleksii Verbytskyi <alverba88@gmail.com>
2022-08-10 13:38:46 +0300

    added temp files for calendar

---
+++ src/components/Calendar/CalendarSidebar/CalenderSidebar.js
@@ -0,0 +1,14 @@
+import styles from './CalenderSidebar.module.css';
+import React from 'react';
+import CreateEventButton from './CreateEventButton';
+import SmallCalendar from './SmallCalendar';
+import Labels from './Labels';
+export const CalendarSidebar = () => {
+  return (
+    <aside className="border p-5 w-64">
+      <CreateEventButton />
+      <SmallCalendar />
+      <Labels />
+    </aside>
+  );
+};
---
+++ src/components/Calendar/SmallCalendar/index.js
@@ -0,0 +1,1 @@
+export { SmallCalendar } from './SmallCalendar';
